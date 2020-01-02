66eeeb5e709f7eda78907caeeb7ade1432e4ff4a
bethgranade <bgranado488@gmail.com>
2020-01-02 12:27:32 -0600

    fixed pw for SS add

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'password',
+  password: 'Summer#611',
   database: 'bamazon'
 });
 
@@ -22,67 +22,63 @@ function readProducts() {
   });
 }
 
-// function createProduct() {
-//   console.log("Inserting a new product...\n");
-//   var query = connection.query(
-//     "INSERT INTO products SET ?",
-//     {
-//       flavor: "Rocky Road",
-//       price: 3.0,
-//       quantity: 50
-//     },
-//     function(err, res) {
-//       if (err) throw err;
-//       console.log(res.affectedRows + " product inserted!\n");
-//       // Call updateProduct AFTER the INSERT completes
-//       updateProduct();
-//     }
-//   );
+ function createProduct() {
+   console.log("Inserting a new product...\n");
+   var query = connection.query(
+     "INSERT INTO products SET ?",
+     {
+       flavor: "Rocky Road",
+       price: 3.0,
+       quantity: 50
+     },
+     function(err, res) {
+       if (err) throw err;
+       console.log(res.affectedRows + " product inserted!\n");
+       // Call updateProduct AFTER the INSERT completes
+       updateProduct();
+     }
+   );
 
   // logs the actual query being run
-//   console.log(query.sql);
-// }
+   console.log(query.sql);
+ }
 
-// function updateProduct() {
-//   console.log("Updating all Rocky Road quantities...\n");
-//   var query = connection.query(
-//     "UPDATE products SET ? WHERE ?",
-//     [
-//       {
-//         quantity: 100
-//       },
-//       {
-//         flavor: "Rocky Road"
-//       }
-//     ],
-//     function(err, res) {
-//       if (err) throw err;
-//       console.log(res.affectedRows + " products updated!\n");
-//       // Call deleteProduct AFTER the UPDATE completes
-//       deleteProduct();
-//     }
-//   );
+ function updateProduct() {
+   console.log("Updating all Rocky Road quantities...\n");
+   var query = connection.query(
+     "UPDATE products SET ? WHERE ?",
+     [
+       {
+         quantity: 100
+       },
+       {
+         flavor: "Rocky Road"
+       }
+     ],
+     function(err, res) {
+       if (err) throw err;
+       console.log(res.affectedRows + " products updated!\n");
+       // Call deleteProduct AFTER the UPDATE completes
+       deleteProduct();
+     }
+   );
 
 //   // logs the actual query being run
-//   console.log(query.sql);
-// }
+   console.log(query.sql);
+ }
 
-// function deleteProduct() {
-//   console.log("Deleting all strawberry icecream...\n");
-//   connection.query(
-//     "DELETE FROM products WHERE ?",
-//     {
-//       flavor: "strawberry"
-//     },
-//     function(err, res) {
-//       if (err) throw err;
-//       console.log(res.affectedRows + " products deleted!\n");
-//       // Call readProducts AFTER the DELETE completes
-//       readProducts();
-//     }
-//   );
-// }
-
-
-
-connection.end();
+ function deleteProduct() {
+   console.log("Deleting all strawberry icecream...\n");
+   connection.query(
+     "DELETE FROM products WHERE ?",
+     {
+       flavor: "strawberry"
+     },
+     function(err, res) {
+       if (err) throw err;
+       console.log(res.affectedRows + " products deleted!\n");
+       // Call readProducts AFTER the DELETE completes
+       readProducts();
+     }
+   );
+ }
